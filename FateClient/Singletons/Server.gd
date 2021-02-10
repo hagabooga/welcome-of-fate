4263ea544e8a44509f1d046f80d823410218da50
Hagabooga <hagaboogaplease@gmail.com>
2021-02-10 14:24:39 -0800

    Remove Authentication and Gateway

--- FateClient/Singletons/Server.gd
+++ FateClient/Singletons/Server.gd
@@ -4,14 +4,13 @@ var network = NetworkedMultiplayerENet.new()
 var ip = "127.0.0.1"
 var port = 1909
 
-# func _ready():
-# 	connect_to_server()
+func _ready():
+	connect_to_server()
 
 
 func connect_to_server():
 	network.create_client(ip, port)
 	get_tree().set_network_peer(network)
-
 	network.connect("connection_succeeded", self, "connection_succeeded")
 	network.connect("connection_failed", self, "connection_failed")
 
